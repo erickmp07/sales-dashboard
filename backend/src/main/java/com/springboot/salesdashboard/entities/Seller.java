feba6949fa55c3a44002affaca47d2a0cb73e472
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2026-06-12 21:08:00 -0300

    Domain, model, database seed

---
+++ backend/src/main/java/com/springboot/salesdashboard/entities/Seller.java
@@ -0,0 +1,51 @@
+package com.springboot.salesdashboard.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_sellers")
+public class Seller {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "seller")
+    private List<Sale> sales = new ArrayList<Sale>();
+
+    public Seller() { }
+
+    public Seller(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Sale> getSales() {
+        return sales;
+    }
+}
